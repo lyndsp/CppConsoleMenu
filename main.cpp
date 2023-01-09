faa329fc8fc2b8f8e3b9a21877300ca4d3e4cb89
lyndsayp <lyndsayp@gmail.com>
2023-01-09 21:47:21 +0000

    Clear screen using unrecommend approach.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include "Domain/Operation.h"
 
+using namespace std;
+
 int main() {
 
     Operation *operation = new Operation();
 
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << operation->Menu() << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << " - " << operation->Menu() << endl;
+    cout << endl;
+
+    while (true) {
+        string input;
+        getline(cin, input);
+
+        if (strcmp(input.c_str(), "q") == 0) {
+            exit(0);
+        }
+        else {
+            system("clear");
+
+            cout << "Hello, World!" << endl;
+            cout << " - " << operation->Menu() << endl;
+            cout << endl;
+        }
+    }
 
     return 0;
 }
